c02fa67d1fa60573a76ce9d0212bbc9a759ad364
Mattia-Fasulo <mattia.fasulo@gmail.com>
2022-12-16 09:57:34 +0100

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,8 @@
+import './bootstrap';
+import '~resources/scss/app.scss';
+import * as bootstrap from 'bootstrap';
+
+// serve a Vit per sapere dove sono le immagini
+import.meta.glob([
+    '../img/**'
+])
